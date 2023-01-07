724e5af4588eccecb6f0bdf67d8425dad367edb2
renaldy <renalraion@gmail.com>
2023-01-07 11:33:31 +0700

    adding logic for ui onboarding

---
+++ app/src/main/java/com/redveloper/rebator/domain/usecase/onboarding/SetOnBoardingUseCase.kt
@@ -0,0 +1,7 @@
+package com.redveloper.rebator.domain.usecase.onboarding
+
+import com.redveloper.rebator.data.local.preference.onboarding.OnBoardingPreference
+
+class SetOnBoardingUseCase(private val onBoardingPreference: OnBoardingPreference) {
+
+}
--- app/src/main/java/com/redveloper/rebator/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/redveloper/rebator/ui/onboarding/OnBoardingActivity.kt
@@ -4,12 +4,15 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.redveloper.rebator.databinding.ActivityOnBoarding2Binding
 import com.redveloper.rebator.ui.BaseActivity
+import com.redveloper.rebator.ui.login.LoginActivity
 import com.redveloper.rebator.ui.onboarding.screen.FirstScreenOnBoarding
 import com.redveloper.rebator.ui.onboarding.screen.SecondScreenOnBoarding
 import com.redveloper.rebator.ui.onboarding.screen.ThirdScreenOnBoarding
+import com.redveloper.rebator.utils.setVisility
 
 class OnBoardingActivity : BaseActivity<ActivityOnBoarding2Binding>() {
 
@@ -27,8 +30,20 @@ class OnBoardingActivity : BaseActivity<ActivityOnBoarding2Binding>() {
         val adapter = OnBoardingPagerAdapter(listFragment, supportFragmentManager, lifecycle)
         binding.viewpager.adapter = adapter
 
-        TabLayoutMediator(binding.tablayout, binding.viewpager){ tab, position ->
-        }.attach()
+        TabLayoutMediator(binding.tablayout, binding.viewpager){ tab, position -> }.attach()
+        binding.tablayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.tablayout.setVisility(tab?.position != 2)
+                binding.btnToLogin.setVisility(tab?.position == 2)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+
+        binding.btnToLogin.setOnClickListener {
+            LoginActivity.navigate(activity = this, finish = true)
+        }
     }
 
     companion object{
