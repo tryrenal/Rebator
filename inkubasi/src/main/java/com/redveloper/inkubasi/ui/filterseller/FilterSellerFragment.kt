06df76b9038698918b62856d9b09f5dc9faae784
renaldy <renalraion@gmail.com>
2023-01-24 03:33:09 +0700

    add adapter gender and status filter

--- inkubasi/src/main/java/com/redveloper/inkubasi/ui/filterseller/FilterSellerFragment.kt
+++ inkubasi/src/main/java/com/redveloper/inkubasi/ui/filterseller/FilterSellerFragment.kt
@@ -1,16 +1,22 @@
 package com.redveloper.inkubasi.ui.filterseller
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.redveloper.inkubasi.R
 import com.redveloper.inkubasi.databinding.FragmentFilterSellerBinding
 import com.redveloper.inkubasi.ui.InkubasiBaseFragment
 import com.redveloper.rebator.design.popup.SingleSelectedPopUp
+import com.redveloper.rebator.domain.entity.Gender
+import com.redveloper.rebator.domain.entity.StatusSeller
+import com.redveloper.rebator.utils.mapper.GenderMapper
+import com.redveloper.rebator.utils.mapper.StatusSellerMapper
 import com.redveloper.rebator.utils.setVisility
 import com.redveloper.rebator.utils.toast
 import com.redveloper.rebator.utils.visible
@@ -20,6 +26,8 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class FilterSellerFragment : InkubasiBaseFragment<FragmentFilterSellerBinding>() {
 
     val filterViewModel: FilterSellerViewModel by viewModel()
+    lateinit var genderAdapter: FilterCardChooseAdapter
+    lateinit var statusAdapter: FilterCardChooseAdapter
 
     override fun inflate(
         inflater: LayoutInflater,
@@ -37,6 +45,41 @@ class FilterSellerFragment : InkubasiBaseFragment<FragmentFilterSellerBinding>()
 
     private fun initView(){
         binding.appbar.btnReset.visible()
+        setupRecyclerGender()
+        setupRecyclerStatus()
+    }
+
+    private fun setupRecyclerGender(){
+        genderAdapter = FilterCardChooseAdapter()
+        genderAdapter.submitList(listOf(
+            Pair(Gender.MAN.name, GenderMapper.getValueOfGender(Gender.MAN)),
+            Pair(Gender.WOMAN.name, GenderMapper.getValueOfGender(Gender.WOMAN)),
+        ))
+        binding.recyclerGender.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = genderAdapter
+        }
+
+        genderAdapter.selectionChanged = {
+            Log.i("dataGender", genderAdapter.itemSelected.toString())
+        }
+    }
+
+    private fun setupRecyclerStatus(){
+        statusAdapter = FilterCardChooseAdapter()
+        statusAdapter.submitList(listOf(
+            Pair(StatusSeller.DRAFT.name, StatusSellerMapper.getValueOfStatus(StatusSeller.DRAFT)),
+            Pair(StatusSeller.CONTACTED.name, StatusSellerMapper.getValueOfStatus(StatusSeller.CONTACTED)),
+            Pair(StatusSeller.VISITED.name, StatusSellerMapper.getValueOfStatus(StatusSeller.VISITED)),
+        ))
+        binding.recyclerStatus.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = statusAdapter
+        }
+
+        statusAdapter.selectionChanged = {
+            Log.i("dataStatus", statusAdapter.itemSelected.toString())
+        }
     }
 
     private fun initClicklistener(){
@@ -61,6 +104,8 @@ class FilterSellerFragment : InkubasiBaseFragment<FragmentFilterSellerBinding>()
                 filterViewModel.getDistrict(idCity)
             }
         }
+
+
     }
 
     private fun initObserver(){
